1cb798301949d74ae7c12a20ccb4cd2e09169af2
alsy4 <syahiralimi@gmail.com>
2026-03-27 08:24:32 +0000

    adjusted LED pin

--- Assignment1/include/LED.h
+++ Assignment1/include/LED.h
@@ -2,9 +2,9 @@
  
 #include <Arduino.h>
  
-const int RED_LED_PIN    = 6;
-const int YELLOW_LED_PIN = 9;
-const int GREEN_LED_PIN  = 12;
+const int RED_LED_PIN    = 3;
+const int YELLOW_LED_PIN = 4;
+const int GREEN_LED_PIN  = 5;
  
 void solidLED();
 void blinkLED();
